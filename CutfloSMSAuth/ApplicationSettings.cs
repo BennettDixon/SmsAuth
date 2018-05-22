10ab9641588864acff4da1c91b67c1524a34dc06
BennettDixon <bennettdixon16@gmail.com>
2018-05-22 14:36:03 -0400

    updated readme, added blank ApplicationSettings.cs

---
+++ CutfloSMSAuth/ApplicationSettings.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+public class ApplicationSettings
+{
+    public static string TwilioAccountSid { get; internal set; }
+    public static string TwilioAuthToken { get; internal set; }
+    public static string TwilioPhoneNumber { get; internal set; }
+    public static string DefaultName { get; internal set; }
+    public static string DefaultNumber { get; internal set; }
+    public static string MailGunKey { get; internal set; }
+    public static string MailGunBaseUrl { get; internal set; }
+
+    public static string GetConnectionString()
+    {
+        return "yourAzureConnectionString";
+    }
+
+    public ApplicationSettings()
+	{
+        TwilioAccountSid = "yourTwilioSid";
+        TwilioAuthToken = "yourTwilioAuthToken";
+        TwilioPhoneNumber = "yourTwilioPhoneNumber";
+        DefaultName = "Default";
+        DefaultNumber = "+12345678900";
+        MailGunKey = "yourMailGunKey";
+        MailGunBaseUrl = "yourMailGunBaseUrl";
+	}
+
+    
+}
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-"# SmsAuth" 
+Welcome to SmsAuth, an Open Source solution for login/registration flow allowing developers to customize their databases and take control of their data. Don't let Google or FaceBook own your users with OAuth, use SmSAuth and build your own database.
+
+View the powerpoint in /configurationDiagrams to get started with setup.
+
+COMING SOON:
+Video tutorial
+Better Security
